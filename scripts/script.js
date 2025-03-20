b11c8b64b97013ecac718e8ae6983560b6478496
Lisa <lisamandemakers@hotmail.com>
2025-03-20 16:50:34 +0100

    change font

--- scripts/script.js
+++ scripts/script.js
@@ -3,25 +3,25 @@ console.log("JavaScript is connected!");
 
 // ONDERSTAANDE CODE MAG JE GEBRUIKEN
 
-const ranges = document.querySelectorAll('[type="range"]');
+// const ranges = document.querySelectorAll('[type="range"]');
 
-const updateRange = range => {
-	// the name of the custom property is the name of the input
-	const rangeName = range.name;
-	// the value of the custom property is the value of the input
-    console.log(rangeName);
-	const rangeValue = range.vconalue;
-	// a custom property is set on the HTML element
-	document.documentElement.style.setProperty("--"+rangeName, rangeValue);
-}
+// const updateRange = range => {
+// 	// the name of the custom property is the name of the input
+// 	const rangeName = range.name;
+// 	// the value of the custom property is the value of the input
+//     console.log(rangeName);
+// 	const rangeValue = range.vconalue;
+// 	// a custom property is set on the HTML element
+// 	document.documentElement.style.setProperty("--"+rangeName, rangeValue);
+// }
 
-ranges.forEach(range => {
-	// the custom property is set initially after loading the document
-	updateRange(range);
+// ranges.forEach(range => {
+// 	// the custom property is set initially after loading the document
+// 	updateRange(range);
 	
-	// the custom property is updated when the input is changed
-	range.oninput = () => {
-		updateRange(range)
-	};
-});
+// 	// the custom property is updated when the input is changed
+// 	range.oninput = () => {
+// 		updateRange(range)
+// 	};
+// });
 
